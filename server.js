bb2ed16dd372836baea6ed66bfc24ad55a0e8ebd
Bennett Rogers <bennettrogers@gmail.com>
2015-08-16 17:23:10 -0400

    adding initial routes and handlers

--- server.js
+++ server.js
@@ -2,18 +2,37 @@ var Hapi = require('hapi');
 
 var server = new Hapi.Server();
 server.connection({
-    host: '0.0.0.0',
+    host: 'localhost',
     port: 8080
 });
 
-server.route({
-    method: 'POST',
-    path: '/',
-    handler: function(request, reply) {
-        console.log('Hello World!');
-        reply('Reply World!');
+var postHandler = function(request, reply) {
+    console.log('postHandler triggered');
+    console.log(request.payload);
+    reply('Webhook received');
+};
+
+var bitbucketPostConfig = {
+    //TODO validate payload has proper secret
+    handler: postHandler
+};
+var githubPostConfig = {
+    //TODO validate payload has proper secret
+    handler: postHandler
+};
+
+server.route([
+    {
+        method: 'POST',
+        path: '/bitbucket',
+        config: bitbucketPostConfig
+    },
+    {
+        method: 'POST',
+        path: '/github',
+        config: githubPostConfig
     }
-});
+]);
 
 server.start(function() {
     console.log('Server running at: ' + server.info.uri);
