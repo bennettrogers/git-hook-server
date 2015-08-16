6b0c4891ed41714c900916c923e0f242e41edf2c
Bennett Rogers <bennettrogers@gmail.com>
2015-08-16 19:47:43 -0400

    simple handling of push webhooks from bitbucket and github

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var Hapi = require('hapi');
+var Joi = require('joi');
 
 var server = new Hapi.Server();
 server.connection({
@@ -6,19 +7,47 @@ server.connection({
     port: 8080
 });
 
-var postHandler = function(request, reply) {
-    console.log('postHandler triggered');
-    console.log(request.payload);
-    reply('Webhook received');
+var githubPushHandler = function(request, reply) {
+    console.log('github push received');
+    var repoUrl = request.payload.repository.html_url;
+    processPush(repoUrl);
+    reply();
 };
+
+var bitbucketPushHandler = function(request, reply) {
+    console.log('bitbucket push received');
+    var repoUrl = request.payload.repository.links.html.href;
+    processPush(repoUrl);
+    reply();
+};
+
+var processPush = function(repoUrl) {
+    console.log(repoUrl);
+}
 
 var bitbucketPostConfig = {
     //TODO validate payload has proper secret
-    handler: postHandler
+    validate: {
+        options: {
+            allowUnknown: true,
+        },
+        headers: {
+            'x-event-key': Joi.string().required().valid('repo:push')
+        }
+    },
+    handler: bitbucketPushHandler
 };
 var githubPostConfig = {
     //TODO validate payload has proper secret
-    handler: postHandler
+    validate: {
+        options: {
+            allowUnknown: true,
+        },
+        headers: {
+            'x-github-event': Joi.string().required().valid('push')
+        }
+    },
+    handler: githubPushHandler
 };
 
 server.route([
